5f9351252fa902e326644473afb9d9b0c730f96e
NevMem <memlolkek@gmail.com>
2019-03-23 01:16:25 +0300

    Concert class update

--- src/data/Concert.js
+++ src/data/Concert.js
@@ -1,13 +1,17 @@
 class Concert {
     constructor() {
         this.name = undefined
-        this.artists = []
+        this.artists = [  ] // Artists ids
         this.location = {
             name: undefined,
             coords: {
-                
+                longitude: undefined,
+                latitude: undefined
             }
         }
+        this.isActive = false
+        this.date = undefined
+        this.concertId = undefined
     }
 }
 
