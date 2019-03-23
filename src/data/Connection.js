165b0f53ead897ec734fcd83f14d9b4cf477255d
NevMem <memlolkek@gmail.com>
2019-03-23 10:37:41 +0300

    Fuck up

--- src/data/Connection.js
+++ src/data/Connection.js
@@ -2,10 +2,7 @@ class Connection {
     constructor(socket_) {
         this.socket = socket_
         this.authenticated = false
-<<<<<<< HEAD
         this.userId = undefined
-=======
->>>>>>> 60e026ad9df6f8d31418951f6925da2779f8f103
     }
 }
 
